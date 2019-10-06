8932d4b76b267b1b2b170eef495a6fa72afb0bc0
Amaury Wyart <amauryw@bam.tech>
2019-10-06 11:50:30 +0200

    ✅(*): enable storyshot in one file

--- frontend/src/components/Button/Button.stories.tsx
+++ frontend/src/components/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { boolean, color, object, select, text, withKnobs } from '@storybook/addon-knobs';
+import { boolean, text, withKnobs } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
 import Button from './Button';

7aba3223096e8ad9403421a1f1c0acd065230109
Amaury Wyart <amauryw@bam.tech>
2019-10-08 01:16:41 +0200

    ✨(Comments): add basic loading middle page

--- frontend/src/pages/Comments/Comments.wrap.tsx
+++ frontend/src/pages/Comments/Comments.wrap.tsx
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import Comments from './Comments';
-import { getComments } from 'redux/Comment/comment.selectors';
+import { getComments, isCommentLoading } from 'redux/Comment/comment.selectors';
 import { loadComments } from 'redux/Comment/comment.actions';
 import { Dispatch } from 'redux';
 import { RootState } from 'redux/types';
 
 const mapStateToProps = (state: RootState) => ({
   comments: getComments(state),
+  isCommentLoading: isCommentLoading(state),
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({

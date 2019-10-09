603fdcab532a769a3cfe87f37244268b8ac2c65d
Amaury Wyart <amauryw@bam.tech>
2019-10-09 23:32:19 +0200

    ✨(Tags): add middle screen loader

--- frontend/src/pages/Tags/Tags.tsx
+++ frontend/src/pages/Tags/Tags.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
-
+import { LoaderContainer } from './Tags.style';
+import Loader from 'components/Loader';
 import StyledTags from './Tags.style';
 import { TagType } from 'redux/Tag';
 
@@ -13,10 +14,16 @@ interface IProps {
 const Tags = React.memo<IProps>(props => {
   useEffect(() => {
     props.loadTags();
+    // eslint-disable-next-line
   }, []);
 
   return (
     <StyledTags>
+      {props.isTagLoading && (
+        <LoaderContainer>
+          <Loader />
+        </LoaderContainer>
+      )}
       <FormattedMessage id="tags.text" />
       {JSON.stringify(props.tags)}
     </StyledTags>

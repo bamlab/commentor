44d29d403491a71d4b943bc601f6d428c5f84470
Amaury Wyart <amauryw@bam.tech>
2019-10-13 19:03:59 +0200

    ✨(Graph): add pie chart exemple on page

--- frontend/src/pages/Graphs/Graphs.style.ts
+++ frontend/src/pages/Graphs/Graphs.style.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
+import { Doughnut } from 'react-chartjs-2';
 
 export const StyledGraphs = styled.div`
   display: flex;
+  flex: 1 0 auto;
   flex-direction: column;
 `;
+
+export const PieChart = styled(Doughnut)``;
--- frontend/src/pages/Graphs/Graphs.tsx
+++ frontend/src/pages/Graphs/Graphs.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { StyledGraphs } from './Graphs.style';
+import { StyledGraphs, PieChart } from './Graphs.style';
 
 interface IProps {}
 
 const Graphs = React.memo<IProps>(props => {
-  return <StyledGraphs>COUCOU</StyledGraphs>;
+  const data = {
+    labels: ['✅', '♻️', '🛠', '🏗', '🤩'],
+    datasets: [
+      {
+        data: [100, 50, 100, 30, 20],
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#e21313', '#faa'],
+        hoverBackgroundColor: ['#aa6384', '#aaA2EB', '#aaCE56', '#aa1313', '#aaa'],
+      },
+    ],
+  };
+
+  return (
+    <StyledGraphs>
+      <PieChart data={data} />
+    </StyledGraphs>
+  );
 });
 
 export default Graphs;

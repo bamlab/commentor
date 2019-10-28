21f682b4172a75789e75fedfabc1f6f914ed748b
Amaury Wyart <amauryw@bam.tech>
2019-10-28 23:59:27 +0100

    🛠(): remove unused import

--- frontend/src/pages/Graphs/Graphs.wrap.tsx
+++ frontend/src/pages/Graphs/Graphs.wrap.tsx
@@ -6,8 +6,6 @@ import { loadTags } from 'redux/Tag/tag.actions';
 import { getTags } from 'redux/Tag/tag.selectors';
 import { getComments } from 'redux/Comment/comment.selectors';
 import { loadComments } from 'redux/Comment/comment.actions';
-import { TagType } from 'redux/Tag';
-import { CommentType } from 'redux/Comment';
 
 import { getSelectedRepositoryIds } from 'redux/Filters';
 

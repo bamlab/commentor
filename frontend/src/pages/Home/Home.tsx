de7c8f6f7cb993e09ee923f980e86b7c674852ed
Maxime Sraïki <maxime.sraiki@gmail.com>
2019-12-25 22:43:38 +0100

    :sparkles: (Home page) Get code from query params if it exists

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
+import queryString from 'query-string';
 import {
   HomeContainer,
   Logo,
@@ -14,11 +15,16 @@ import logo from 'assets/final_low.png';
 
 type PropsType = {
   loadRepositories: () => void;
+  location: { search: string };
 };
 
 const Home = React.memo<PropsType>(props => {
   useEffect(
     () => {
+      const params = queryString.parse(props.location.search);
+      if (params.code) {
+        props.setAccessToken(params.code);
+      }
       props.loadRepositories();
     },
     [props],
@@ -34,7 +40,7 @@ const Home = React.memo<PropsType>(props => {
         <GithubAuthentTitleWrapper>
           <FormattedMessage id="home.authenticate-via-github" />
         </GithubAuthentTitleWrapper>
-        <GithubAuthentButton href="https://github.com/login/oauth/authorize?client_id=Iv1.2125db5cc55ea22c&state=coucou">
+        <GithubAuthentButton href="https://github.com/login/oauth/authorize?client_id=Iv1.2125db5cc55ea22c">
           Login via Github
         </GithubAuthentButton>
       </GithubAuthentWrapper>

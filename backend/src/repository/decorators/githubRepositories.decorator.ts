29714812e66b6798c1c26f0e479ed03005e9854e
Maxime Sraïki <maxime.sraiki@gmail.com>
2019-12-29 22:44:32 +0100

    :recycle: (Github repositories getter) Move github repositories getter to decorator

---
+++ backend/src/repository/decorators/githubRepositories.decorator.ts
@@ -0,0 +1,58 @@
+import { createParamDecorator } from '@nestjs/common';
+import { GithubAnswer } from '../interfaces/repository.dto';
+import * as request from 'request-promise';
+import { GithubRepository } from '../interfaces/repository.dto';
+
+const queryPaginatedGithubRepositories = async (
+  userAccessToken: string,
+  previousPageRepositories: GithubRepository[] = [],
+  previousPageCursor?: string,
+): Promise<GithubRepository[]> => {
+  const query = `
+    query {
+      viewer {
+        repositories(first: 100, ownerAffiliations: [OWNER, COLLABORATOR, ORGANIZATION_MEMBER] ${
+          previousPageCursor ? `, after:"${previousPageCursor}"` : ''
+        }) {
+          totalCount
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+          nodes {
+            databaseId,
+            name
+          }
+        }
+      }
+    }
+  `;
+
+  const githubAnswer: GithubAnswer = await request({
+    uri: 'https://api.github.com/graphql',
+    headers: {
+      Authorization: `bearer ${userAccessToken}`,
+      'User-Agent': 'Request-Promise',
+    },
+    method: 'POST',
+    json: true,
+    body: {
+      query,
+    },
+  });
+
+  const repositoriesList = previousPageRepositories.concat(
+    githubAnswer.data.viewer.repositories.nodes,
+  );
+  const pageInfo = githubAnswer.data.viewer.repositories.pageInfo;
+  if (pageInfo.hasNextPage) {
+    return queryPaginatedGithubRepositories(userAccessToken, repositoriesList, pageInfo.endCursor);
+  }
+  return repositoriesList;
+};
+
+export const GithubRepositories = createParamDecorator(async (_, req) => {
+  if (req.cookies.access_token) {
+    return queryPaginatedGithubRepositories(req.cookies.access_token);
+  }
+});

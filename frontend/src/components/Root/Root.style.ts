a889ab2675267f2fa97ea22d5f299b162c77b8f8
Maxime Sraïki <maxime.sraiki@gmail.com>
2020-01-13 22:43:13 +0100

    :fire: (Root component) Remove unwanted background

--- frontend/src/components/Root/Root.style.ts
+++ frontend/src/components/Root/Root.style.ts
@@ -5,14 +5,11 @@ export const RootContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
-  padding: 0 ${getSpacing(26)};
+  padding: 0 ${getSpacing(2)};
 `;
 RootContainer.displayName = 'RootContainer';
 
 export const PageContent = styled.main`
-  background-color: ${colorUsage.contentBackground};
-  border-radius: ${borderRadius.large};
-  padding: ${getSpacing(6)};
   flex-grow: 1;
 `;
 PageContent.displayName = 'PageContent';

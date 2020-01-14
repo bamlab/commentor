2cc080254049f44ac3c72a10cf847d53d95cf5f2
Amaury Wyart <amauryw@bam.tech>
2020-01-14 22:13:24 +0100

    ✨(*) : add theme color

--- frontend/src/components/Root/Root.style.ts
+++ frontend/src/components/Root/Root.style.ts
@@ -5,12 +5,12 @@ export const RootContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
-  background-color: #5b855f;
   padding: 0 ${getSpacing(2)};
 `;
 RootContainer.displayName = 'RootContainer';
 
 export const PageContent = styled.main`
   flex-grow: 1;
+  box-shadow: 0.5em -0.3em 10em rgb(242, 42, 51, 0.24);
 `;
 PageContent.displayName = 'PageContent';

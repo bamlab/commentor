a0105c5c06cbd90ede5a2b5b7a838b38798f0a05
Amaury Wyart <amauryw@bam.tech>
2020-01-20 22:31:26 +0100

    ✨(BarChart): create random graphe component

---
+++ frontend/src/components/BarChart/BarChart.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BarChartContainer } from './BarChart.style';
+
+interface propTypes {}
+
+const BarChart: React.FunctionComponent<propTypes> = props => {
+  return <div>fwefwe</div>;
+};
+
+export default BarChart;

fafe60da009363ceb7a679395db6bea2a4631d5c
Maxime Sraïki <maxime.sraiki@gmail.com>
2020-01-27 13:06:10 +0100

    :recycle: (Graphes) make the toggle component dumb

--- frontend/src/components/Toggle/Toggle.tsx
+++ frontend/src/components/Toggle/Toggle.tsx
@@ -4,19 +4,23 @@ import { ToggleContainer } from './Toggle.style';
 interface propTypes {
   onSelect: (value: string) => void;
   value: string;
+  firstOption: string;
+  secondOption: string;
 }
 
 const Toggle: React.FunctionComponent<propTypes> = props => {
   const { value, onSelect } = props;
 
-  const selectBarChart = () => onSelect('BAR_CHART');
-  const selectDonutChart = () => onSelect('DONUT_CHART');
+  const selectFirstOption = () => onSelect(props.firstOption);
+  const selectSecondOption = () => onSelect(props.secondOption);
   const isSelected = (containerValue: string): boolean => containerValue === value;
   return (
     <ToggleContainer>
-      <div onClick={selectBarChart}>{isSelected('BAR_CHART') ? 'bar-active' : 'bar'}</div>
-      <div onClick={selectDonutChart}>
-        {isSelected('DONUT_CHART') ? 'doneught-active' : 'doneught'}
+      <div onClick={selectFirstOption}>
+        {isSelected(props.firstOption) ? `${props.firstOption}-active` : `${props.firstOption}`}
+      </div>
+      <div onClick={selectSecondOption}>
+        {isSelected(props.secondOption) ? `${props.secondOption}-active` : `${props.secondOption}`}
       </div>
     </ToggleContainer>
   );

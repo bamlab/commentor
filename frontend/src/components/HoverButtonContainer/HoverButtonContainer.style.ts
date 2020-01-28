0e6be0e5b5da797c29f925c3b0b008ae5b7e7d30
Amaury Wyart <amauryw@bam.tech>
2020-01-28 22:48:40 +0100

    ✨(HoverButtonContainer): channge hover design

--- frontend/src/components/HoverButtonContainer/HoverButtonContainer.style.ts
+++ frontend/src/components/HoverButtonContainer/HoverButtonContainer.style.ts
@@ -3,13 +3,17 @@ import { colorUsage } from 'stylesheet';
 
 const HOVER_SQUARE_SIZE = 30;
 
+/**
+ * Component used for container icon in GenericTable only
+ */
 export const HoverButtonContainerStyled = styled.div`
   cursor: pointer;
   display: flex;
   justify-content: center;
   align-items: center;
   :hover {
-    background-color: ${colorUsage.primaryButtonBackgroundHover};
+    box-shadow: 1px 2px 2px 1px ${colorUsage.primaryButtonBackgroundHover};
+
     border-radius: 5px;
     width: ${HOVER_SQUARE_SIZE}px;
     height: ${HOVER_SQUARE_SIZE}px;

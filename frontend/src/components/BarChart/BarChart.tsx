a78d143592481f21bb97ea854171deee10805395
Maxime Sraïki <maxime.sraiki@gmail.com>
2020-01-28 20:31:53 +0100

    :sparkles: (graphs) add new bar chart

--- frontend/src/components/BarChart/BarChart.tsx
+++ frontend/src/components/BarChart/BarChart.tsx
@@ -1,10 +1,77 @@
 import React from 'react';
 import { BarChartContainer } from './BarChart.style';
+import { VictoryChart, VictoryBar, VictoryTheme, VictoryAxis, VictoryStack } from 'victory';
+import { colorUsage, fontFamily, fontSize } from 'stylesheet';
+import { map, chain } from 'lodash';
+import { TagType } from 'redux/Tag';
 
-interface propTypes {}
+interface propTypes {
+  data: { x: number | string; y: number; y0: number; tag: TagType }[];
+}
 
 const BarChart: React.FunctionComponent<propTypes> = props => {
-  return <div>fwefwe</div>;
+  const ticksLabelsStyle = {
+    fill: colorUsage.primaryTextColor,
+    fontSize: fontSize.XXSmall,
+    fontFamily: fontFamily.main,
+  };
+
+  return (
+    <BarChartContainer>
+      {props.data && props.data.length > 0 && (
+        <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
+          <VictoryAxis
+            dependentAxis
+            tickCount={Math.max(
+              ...chain(props.data)
+                .countBy('x')
+                .map((countByDate: number) => countByDate)
+                .value(),
+            )}
+            style={{
+              axis: { stroke: 'none' },
+              tickLabels: ticksLabelsStyle,
+              grid: { stroke: colorUsage.primaryTextColor },
+            }}
+          />
+          <VictoryAxis
+            tickCount={
+              chain(props.data)
+                .map('x')
+                .uniq()
+                .value().length
+            }
+            style={{
+              axis: { stroke: 'none' },
+              tickLabels: ticksLabelsStyle,
+              grid: { stroke: colorUsage.primaryTextColor },
+            }}
+          />
+          <VictoryStack>
+            {map(
+              props.data,
+              (barChartItem: { x: number | string; y: number; y0: number; tag: TagType }) => {
+                return (
+                  <VictoryBar
+                    animate={{
+                      duration: 2000,
+                      onLoad: { duration: 1000 },
+                    }}
+                    style={{
+                      data: {
+                        fill: ({ datum }) => (datum.tag ? datum.tag.color : colorUsage.error),
+                      },
+                    }}
+                    data={[barChartItem]}
+                  />
+                );
+              },
+            )}
+          </VictoryStack>
+        </VictoryChart>
+      )}
+    </BarChartContainer>
+  );
 };
 
 export default BarChart;

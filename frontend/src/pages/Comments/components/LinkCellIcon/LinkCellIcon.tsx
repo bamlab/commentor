67e79c52ac43d8602b7c2fd6e48642d55ed0d214
Amaury Wyart <amauryw@bam.tech>
2020-01-28 21:29:14 +0100

    ✨(LinnkCellIcon): add some style on linkCellIcon

--- frontend/src/pages/Comments/components/LinkCellIcon/LinkCellIcon.tsx
+++ frontend/src/pages/Comments/components/LinkCellIcon/LinkCellIcon.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { GoLinkExternal } from 'react-icons/go';
+import styled from 'styled-components';
+import { colorUsage } from 'stylesheet';
 
+const HOVER_SQUARE_SIZE = 30;
 type propsType = {
   onIconClick: () => void;
 };
+
+const GoLinkExternalButton = styled(GoLinkExternal)`
+  cursor: pointer;
+  :hover {
+    background-color: ${colorUsage.primaryButtonBackgroundHover};
+    border-radius: 5px;
+    width: ${HOVER_SQUARE_SIZE}px;
+    height: ${HOVER_SQUARE_SIZE}px;
+  }
+`;
 
 export const LinkCellIcon: React.FunctionComponent<propsType> = props => {
   const onClick = () => {
     props.onIconClick();
   };
-  return <GoLinkExternal onClick={onClick} />;
+  return <GoLinkExternalButton onClick={onClick} />;
 };

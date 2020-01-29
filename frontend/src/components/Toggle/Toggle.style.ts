40e379fd25ebaaaed7a5f48e55e063680a6ab21a
Maxime Sraïki <maxime.sraiki@gmail.com>
2020-01-29 11:55:50 +0100

    :art: (Design the toggle)

--- frontend/src/components/Toggle/Toggle.style.ts
+++ frontend/src/components/Toggle/Toggle.style.ts
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
+import { getSpacing, fontSize, colorUsage } from 'stylesheet';
 
 export const ToggleContainer = styled.div`
-  background-color: red;
+  margin-top: ${getSpacing(5)};
+  display: flex;
+  flex-direction: row;
+  box-shadow: 2px 2px 2px 2px ${colorUsage.primaryTextColor};
+  border-radius: 5px;
+`;
+
+export const Spacer = styled.div`
+  margin-right: ${getSpacing(5)};
+  margin-left: ${getSpacing(5)};
+  font-size: ${fontSize.XXLarge};
+  color: ${colorUsage.primaryTextColor};
 `;

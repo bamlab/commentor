b11eabe5e43d683215ff404f94d05cf7550379e7
Amaury Wyart <amauryw@bam.tech>
2020-02-18 21:16:35 +0100

    ✨ (Multiselect): add loading state on RepositoryMultiSelect

--- frontend/src/redux/Repository/repository.selectors.ts
+++ frontend/src/redux/Repository/repository.selectors.ts
@@ -3,3 +3,5 @@ import { RepositoryType } from './repository.types';
 
 export const getRepositories = (store: RootState): RepositoryType[] =>
   store.repository.repositories;
+
+export const isRepositoriesLoading = (store: RootState): boolean => store.repository.isLoading;

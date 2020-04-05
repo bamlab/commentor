51d386f469b5c055c5023d7609cbd13f7490dd26
Amaury Wyart <amauryw@bam.tech>
2020-04-05 13:29:46 +0200

    🐛(client): fix bug where comments provided by backend is  stringified

--- frontend/src/services/date/dateFormatter.ts
+++ frontend/src/services/date/dateFormatter.ts
@@ -1,9 +1,14 @@
-import { format } from 'date-fns';
+import { format, parseISO } from 'date-fns';
 
 export type FomattingFunctionType = (date: Date) => string;
 
-export const formatDateToDDMMYYYYSlash = (date: Date) => format(date, 'dd/MM/YYY');
+export const formatDateToDDMMYYYYSlash = (date: Date): string => format(date, 'dd/MM/YYY');
 
-export const formatDateToDDMMYYYLined = (date: Date) => format(date, 'dd-MM-YYY');
+export const formatDateToDDMMYYYLined = (date: Date): string => format(date, 'dd-MM-YYY');
 
-export const formatDateToDDMMLined = (date: Date) => format(date, 'dd-MM');
+export const formatDateToDDMMLined = (date: Date): string => format(date, 'dd-MM');
+
+export const formatStringOrDateToDate = (date: Date | string): Date => {
+  if (typeof date === 'string') return parseISO(date);
+  return date;
+};

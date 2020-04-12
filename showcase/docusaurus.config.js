79a05996e73ceaa815920eedbd5a10c0236988e3
Maxime Sraïki <maxime.sraiki@gmail.com>
2020-04-13 01:42:13 +0200

    :sparkles: (showcase) Design the showcase

--- showcase/docusaurus.config.js
+++ showcase/docusaurus.config.js
@@ -20,7 +20,6 @@ module.exports = {
           label: "Docs",
           position: "left"
         },
-        { to: "blog", label: "Blog", position: "left" },
         {
           href: "https://github.com/bamlab/commentor",
           label: "GitHub",

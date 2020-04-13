dda1c6e72743d43a2f3b35be5fcf78c6325ad56f
Maxime Sraïki <maxime.sraiki@gmail.com>
2020-04-13 17:57:28 +0200

    :memo: (showcase) add trusted by section

--- showcase/docusaurus.config.js
+++ showcase/docusaurus.config.js
@@ -15,7 +15,7 @@ module.exports = {
       },
       links: [
         {
-          to: "docs/doc1",
+          to: "docs/repositoryAccess",
           activeBasePath: "docs",
           label: "Docs",
           position: "left"

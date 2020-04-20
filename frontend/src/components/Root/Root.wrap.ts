a72de227c46277a741046062f3ad8c86a023dd08
Amaury Wyart <amauryw@bam.tech>
2020-04-20 21:18:16 +0200

    ✨(Header): add animation on loading

--- frontend/src/components/Root/Root.wrap.ts
+++ frontend/src/components/Root/Root.wrap.ts
@@ -3,9 +3,11 @@ import Root from './Root';
 import { isAuthenticated } from 'redux/Authentication/authentication.selectors';
 import { RootState } from 'redux/types';
 import { Dispatch } from 'react';
+import { isCommentLoading } from '../../redux/Comment';
 
 const mapStateToProps = (state: RootState) => ({
   isAuthenticated: isAuthenticated(state),
+  isCommentLoading: isCommentLoading(state),
 });
 
 // @ts-ignore Generic type 'Dispatch' requires 1 type argument(s)

714132a85524a7739be6e30e1be0221ae1b7da08
Amaury Wyart <amauryw@bam.tech>
2020-04-20 21:18:16 +0200

    ✨(Header): add animation on loading

--- frontend/src/components/Header/Header.style.ts
+++ frontend/src/components/Header/Header.style.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import { fontStyles, getSpacing, colorUsage } from 'stylesheet';
+import Lottie from 'react-lottie';
 
 const HEADER_HEIGHT = 68;
+const ANIMATION_HORIZONTAL_OFFSET = 18;
 
 export const HeaderContainer = styled.header`
   display: flex;
@@ -19,8 +21,20 @@ export const HeaderContainer = styled.header`
 HeaderContainer.displayName = 'HeaderContainer';
 
 export const Logo = styled.img`
-  padding: 0 ${getSpacing(4)};
-  height: 40px;
+  height: 22px;
+  width: 80px;
+  margin-bottom: -4px;
 `;
 
 Logo.displayName = 'Logo';
+
+export const LogoContainer = styled.div`
+  padding: 0 ${getSpacing(1)};
+`;
+
+export const AnimatedLogoContainer = styled.div`
+  padding: 0 ${ANIMATION_HORIZONTAL_OFFSET}px;
+`;
+export const AnimatedLogo = styled(Lottie)``;
+
+AnimatedLogo.displayName = 'AnimatedLogo';

41cc84a17e207a52ed62a053aaae28f984ec5eda
Amaury Wyart <amauryw@bam.tech>
2020-05-04 15:37:42 +0200

    ✨(comments): Add Promiseall to saga

--- frontend/src/redux/Comment/comment.sagas.ts
+++ frontend/src/redux/Comment/comment.sagas.ts
@@ -1,18 +1,16 @@
-import { call, put, takeEvery, select } from 'redux-saga/effects';
+import { all, call, put, takeEvery, select } from 'redux-saga/effects';
 import client from 'services/networking/client';
 import { ActionType, getType } from 'typesafe-actions';
 import { loadComments, loadPieChartData } from './comment.actions';
-import { CommentType, PieChartData } from './comment.types';
 import { getFilters } from '../Filters';
 
 export function* loadCommentsSaga(action: ActionType<typeof loadComments.request>) {
   try {
     let filters = yield select(getFilters);
-    const comments: CommentType[] = yield call([client, client.fetchComments], filters);
-    const pieChartData: PieChartData[] = yield call(
-      [client, client.fetchPieChartDataComments],
-      filters,
-    );
+    const [comments, pieChartData] = yield all([
+      call([client, client.fetchComments], filters),
+      call([client, client.fetchPieChartDataComments], filters),
+    ]);
 
     yield put(loadComments.success({ comments }));
     yield put(loadPieChartData.success({ pieChartData }));

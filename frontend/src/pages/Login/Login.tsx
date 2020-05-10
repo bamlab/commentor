6dbec749de6c5ff66c674172c27bae7b8431e423
Maxime Sraïki <maxime.sraiki@gmail.com>
2020-05-10 22:29:04 +0200

    :bug: (gitlab authent) fix gitlab authent url

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -74,7 +74,7 @@ const Login = React.memo<LoginPropsType>(props => {
             </GithubAuthentButton>
             <GitlabAuthentButton
               onClick={() => {
-                window.location.href = `https://gitlab.com/oauth/authorize?state=gitlab&response_type=code&scope=read_user+api&redirect_uri=${
+                window.location.href = `https://gitlab.com/oauth/authorize?response_type=code&scope=read_user+api+read_repository&redirect_uri=${
                   process.env.REACT_APP_OAUTH_REDIRECT_URL
                 }&state=gitlab&client_id=${process.env.REACT_APP_GITLAB_APP_CLIENT_ID}`;
               }}

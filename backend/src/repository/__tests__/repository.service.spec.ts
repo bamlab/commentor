23de69dd509959fb7b519ac4929519e65d82db2a
Maxime Sraïki <maxime.sraiki@gmail.com>
2020-06-02 15:51:32 +0200

    :white_check_mark: (repository.service) add more tests to the repository service

--- backend/src/repository/__tests__/repository.service.spec.ts
+++ backend/src/repository/__tests__/repository.service.spec.ts
@@ -3,18 +3,62 @@ import { CommentService } from '../../comment/comment.service';
 
 let repositoryService: RepositoryService;
 
+const mockedCheckIfCommentsExistForRepository = jest
+  .fn()
+  .mockImplementation((id: string) => id === 'idWithExistingComments');
+
 describe('Repository Service', () => {
-  beforeAll(() => {
+  beforeEach(() => {
+    jest.clearAllMocks();
     const mockedCommentService = {
-      checkIfCommentsExistForRepository: jest.fn(),
+      checkIfCommentsExistForRepository: mockedCheckIfCommentsExistForRepository,
     };
     repositoryService = new RepositoryService((mockedCommentService as unknown) as CommentService);
   });
   describe('[Method] getUserCommentedRepositories', () => {
     it('should return an empty list if no providerRepositories are provided', async () => {
-      const userCommentedRepositories = await repositoryService.getUserCommentedRepositories([]);
+      const providerRepositories: any[] = [];
+      const userCommentedRepositories = await repositoryService.getUserCommentedRepositories(
+        providerRepositories,
+      );
       expect(userCommentedRepositories).toEqual([]);
     });
-    it('should return the repositories linked to existing comments in database', () => {});
+    it('should return an empty list if provided repositories is null', async () => {
+      const providerRepositories: any[] = null;
+      const userCommentedRepositories = await repositoryService.getUserCommentedRepositories(
+        providerRepositories,
+      );
+      expect(userCommentedRepositories).toEqual([]);
+    });
+    it('should return the repositories linked to existing comments in database if there are so', async () => {
+      const providerRepositories: any[] = [
+        { id: 'idWithExistingComments', name: 'nameWithExistingComments' },
+        { id: 'idWithoutExistingComments', name: 'nameWithoutExistingComments' },
+      ];
+
+      const expectedFilteredRepositories = [
+        { id: 'idWithExistingComments', name: 'nameWithExistingComments' },
+      ];
+
+      const userCommentedRepositories = await repositoryService.getUserCommentedRepositories(
+        providerRepositories,
+      );
+      expect(mockedCheckIfCommentsExistForRepository).toHaveBeenCalledTimes(2);
+      expect(userCommentedRepositories).toEqual(expectedFilteredRepositories);
+    });
+    it('should return an empty array if no repository has associated comments', async () => {
+      const providerRepositories: any[] = [
+        { id: 'idWithoutExistingComments', name: 'nameWithoutExistingComments' },
+        { id: 'idWithoutExistingComments2', name: 'nameWithoutExistingComments2' },
+      ];
+
+      const expectedFilteredRepositories: any[] = [];
+
+      const userCommentedRepositories = await repositoryService.getUserCommentedRepositories(
+        providerRepositories,
+      );
+      expect(mockedCheckIfCommentsExistForRepository).toHaveBeenCalledTimes(2);
+      expect(userCommentedRepositories).toEqual(expectedFilteredRepositories);
+    });
   });
 });

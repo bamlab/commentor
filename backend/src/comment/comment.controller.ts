b4567cfc810f3790228abb6b11729fbd2ac5aa58
Irina Legkikh <irinal@bam.tech>
2020-06-04 16:22:51 +0200

    :art: (comment) Move chart data extracting function to comment.service

--- backend/src/comment/comment.controller.ts
+++ backend/src/comment/comment.controller.ts
@@ -1,22 +1,12 @@
-import { Controller, Body, Post, Logger } from '@nestjs/common';
-import { isNil, chain, map } from 'lodash';
-
-import {
-  CommentEvent,
-  FiltersType,
-  GetFilteredCommentsAnswer,
-  BarChartData,
-} from './interfaces/comment.dto';
+import { Controller, Body, Post } from '@nestjs/common';
+import { isNil } from 'lodash';
+import { CommentEvent, FiltersType, GetFilteredCommentsAnswer } from './interfaces/comment.dto';
 import { Comment } from './comment.entity';
-import { CommentService } from './comment.service';
+import { CommentService, FIRST_COMMENT_DATE } from './comment.service';
 import { ProviderRepositoriesFilter } from '../auth/decorators/providerRepositoriesFilter.decorator';
-import { Tag } from '../tag/tag.entity';
 import { TagService } from '../tag/tag.service';
-import { filterTagsWithCodes } from './comment.utils';
 import { formatComment as formatGitlabComment } from '../auth/authenticationProviders/gitlab';
 import { formatComment as formatGithubComment } from '../auth/authenticationProviders/github';
-
-const FIRST_COMMENT_DATE = new Date('November 03, 1994 09:24:00');
 
 @Controller('comments')
 export class CommentController {
@@ -55,73 +45,8 @@ export class CommentController {
     filters: FiltersType,
     @ProviderRepositoriesFilter() filteredGithubRepositoriesIds: number[],
   ): Promise<GetFilteredCommentsAnswer> {
-    try {
-      if (
-        filteredGithubRepositoriesIds &&
-        filteredGithubRepositoriesIds.length > 0 &&
-        !isNil(filters.oAuthLogin)
-      ) {
-        // Make a copy of filters.tagCode to avoid shiftin reference array in children
-        const TagCodeFilters = filters.tagCodes.filter(() => true);
-        const fetchedComments = await this.service.getCommentsWithFilters({
-          repositoriesIds: filteredGithubRepositoriesIds,
-          startingDate: isNil(filters.startingDate) ? FIRST_COMMENT_DATE : filters.startingDate,
-          endingDate: isNil(filters.endingDate) ? new Date() : filters.endingDate,
-          requesterIds: filters.requesterIds,
-          commentorIds: filters.commentorIds,
-          tagCodes: TagCodeFilters,
-        });
-
-        const userTags = await this.tagService.getByLogin(filters.oAuthLogin);
-
-        const filteredTags = filterTagsWithCodes(userTags, filters.tagCodes);
-
-        const pieChartFormattedData = chain(filteredTags)
-          .map((tag: Tag) => ({
-            x: tag.code,
-            y: fetchedComments.filter((comment: Comment) => !!comment.body.match(tag.code)).length,
-            tag,
-          }))
-          .filter(chartDatum => chartDatum.y > 0)
-          .value();
-
-        // @ts-ignore well looks like lodash typing failed on this
-        const barChartFormattedData: BarChartData[] = chain(fetchedComments)
-          .groupBy((comment: Comment) => {
-            comment.creationDate.setHours(0, 0, 0, 0);
-            return comment.creationDate;
-          })
-          .map((comments: Comment[]) =>
-            map(comments, (comment: Comment) =>
-              chain(filteredTags)
-                .filter((tag: Tag) => !!comment.body.match(tag.code))
-                .map((tag: Tag) => {
-                  comment.creationDate.setHours(0, 0, 0, 0);
-                  return [{ x: comment.creationDate, y: 1, y0: 0, tag }];
-                })
-                .value(),
-            ),
-          )
-          .flattenDeep()
-          .sortBy('x')
-          .value();
-
-        return {
-          barChartData: barChartFormattedData,
-          pieChartData: pieChartFormattedData,
-          comments: fetchedComments,
-        };
-      } else {
-        return {
-          barChartData: [],
-          pieChartData: [],
-          comments: [],
-        };
-      }
-    } catch (error) {
-      Logger.error(error, `Error on getting filtered data`);
-      throw error;
-    }
+    const userTags = await this.tagService.getByLogin(filters.oAuthLogin);
+    return await this.service.getChartData(userTags, filters, filteredGithubRepositoriesIds);
   }
 
   @Post()
--- backend/src/comment/comment.service.ts
+++ backend/src/comment/comment.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { isNil, chain, map } from 'lodash';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-
-import { Comment } from './comment.entity';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { Repository } from 'typeorm';
+import { BarChartData, FiltersType } from './interfaces/comment.dto';
+import { Comment } from './comment.entity';
 import {
   tagCodesCommentQueryDecorator,
   requesterIdsCommentQueryDecorator,
@@ -11,6 +12,10 @@ import {
   dateFilterCommentQueryDecorator,
   repositoriesIdsFilterCommentQueryDecorator,
 } from './comment.decorator';
+import { filterTagsWithCodes } from './comment.utils';
+import { Tag } from '../tag/tag.entity';
+
+export const FIRST_COMMENT_DATE = new Date('November 03, 1994 09:24:00');
 
 @Injectable()
 export class CommentService extends TypeOrmCrudService<Comment> {
@@ -37,6 +42,78 @@ export class CommentService extends TypeOrmCrudService<Comment> {
     } else {
       await this.commentRepository.save(comment);
       return;
+    }
+  };
+
+  getChartData = async (
+    userTags: Tag[],
+    filters: FiltersType,
+    filteredGithubRepositoriesIds: number[],
+  ) => {
+    try {
+      if (
+        filteredGithubRepositoriesIds &&
+        filteredGithubRepositoriesIds.length > 0 &&
+        !isNil(filters.oAuthLogin)
+      ) {
+        // Make a copy of filters.tagCode to avoid shiftin reference array in children
+        const TagCodeFilters = filters.tagCodes.filter(() => true);
+        const fetchedComments = await this.getCommentsWithFilters({
+          repositoriesIds: filteredGithubRepositoriesIds,
+          startingDate: isNil(filters.startingDate) ? FIRST_COMMENT_DATE : filters.startingDate,
+          endingDate: isNil(filters.endingDate) ? new Date() : filters.endingDate,
+          requesterIds: filters.requesterIds,
+          commentorIds: filters.commentorIds,
+          tagCodes: TagCodeFilters,
+        });
+
+        const filteredTags = filterTagsWithCodes(userTags, filters.tagCodes);
+
+        const pieChartFormattedData = chain(filteredTags)
+          .map((tag: Tag) => ({
+            x: tag.code,
+            y: fetchedComments.filter((comment: Comment) => !!comment.body.match(tag.code)).length,
+            tag,
+          }))
+          .filter(chartDatum => chartDatum.y > 0)
+          .value();
+
+        // @ts-ignore well looks like lodash typing failed on this
+        const barChartFormattedData: BarChartData[] = chain(fetchedComments)
+          .groupBy((comment: Comment) => {
+            comment.creationDate.setHours(0, 0, 0, 0);
+            return comment.creationDate;
+          })
+          .map((comments: Comment[]) =>
+            map(comments, (comment: Comment) =>
+              chain(filteredTags)
+                .filter((tag: Tag) => !!comment.body.match(tag.code))
+                .map((tag: Tag) => {
+                  comment.creationDate.setHours(0, 0, 0, 0);
+                  return [{ x: comment.creationDate, y: 1, y0: 0, tag }];
+                })
+                .value(),
+            ),
+          )
+          .flattenDeep()
+          .sortBy('x')
+          .value();
+
+        return {
+          barChartData: barChartFormattedData,
+          pieChartData: pieChartFormattedData,
+          comments: fetchedComments,
+        };
+      } else {
+        return {
+          barChartData: [],
+          pieChartData: [],
+          comments: [],
+        };
+      }
+    } catch (error) {
+      Logger.error(error, `Error on getting filtered data`);
+      throw error;
     }
   };
 

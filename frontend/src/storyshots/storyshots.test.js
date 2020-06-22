3f741eea8fe0979d240fce7488ac7ddb0d8ea128
Amaury Wyart <amauryw@bam.tech>
2020-06-22 23:41:37 +0200

    fix-test-snapshot

--- frontend/src/storyshots/storyshots.test.js
+++ frontend/src/storyshots/storyshots.test.js
@@ -1,33 +1,20 @@
 import initStoryshots, { Stories2SnapsConverter } from '@storybook/addon-storyshots';
-import { mount } from 'enzyme';
+import { render } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-// Runner
+/**
+ * this is used to split shapshot to different files
+ * @todo  Use screenshot instead of snapshot ( or at least mount, but absolute filename breaks our ci)
+ *  */
 initStoryshots({
-  asyncJest: true, // this is the option that activates the async behaviour
-  test: ({
-    story,
-    context,
-    done, // --> callback passed to test method when asyncJest option is true
-  }) => {
+  test: ({ story, context }) => {
     const converter = new Stories2SnapsConverter();
     const snapshotFilename = converter.getSnapshotFileName(context);
     const storyElement = story.render();
 
-    // mount the story
-    const tree = mount(storyElement);
+    const tree = render(storyElement);
 
-    // wait until the mount is updated, in our app mostly by Relay
-    // but maybe something else updating the state of the component
-    // somewhere
-    const waitTime = 1;
-    setTimeout(() => {
-      if (snapshotFilename) {
-        expect(toJson(tree.update())).toMatchSpecificSnapshot(snapshotFilename);
-      }
-
-      done();
-    }, waitTime);
+    expect(toJson(tree)).toMatchSpecificSnapshot(snapshotFilename);
   },
   // other options here
 });

79ca4a5314f808e54264825668e28bb5409f4ab5
Maxime Sraïki <maxime.sraiki@gmail.com>
2020-06-27 11:33:06 +0200

    :sparkles: (Pie chart) DIsplay tag & percent directly on the pie chart

--- frontend/src/components/PieChart/PieChart.tsx
+++ frontend/src/components/PieChart/PieChart.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { PieChartContainer } from './PieChart.style';
-import { VictoryPie, VictoryTooltip } from 'victory';
+import { VictoryPie, VictoryLabel } from 'victory';
 import { colorUsage, fontStyles } from 'stylesheet';
 import { PieChartData } from '../../redux/Comment';
 
@@ -8,13 +8,17 @@ interface propTypes {
   data: PieChartData[];
 }
 
+const PIE_INNER_RADIUS = 55;
 const PieChart = React.memo<propTypes>(props => {
   const [angle, setAngle] = useState(0);
 
   const ticksLabelsStyle = {
     fill: colorUsage.text,
-    ...fontStyles.small,
+    ...fontStyles.regular,
+    alignItems: 'center',
   };
+
+  const total = props.data.reduce((sum, datum) => sum + datum.y, 0);
 
   useEffect(() => {
     setTimeout(() => {
@@ -26,21 +30,25 @@ const PieChart = React.memo<propTypes>(props => {
     <PieChartContainer>
       {props.data && props.data.length > 0 && (
         <VictoryPie
-          labelComponent={<VictoryTooltip style={{ ...fontStyles.small }} />}
+          labelComponent={<VictoryLabel />}
           height={300}
           width={300}
-          innerRadius={55}
+          innerRadius={PIE_INNER_RADIUS}
           padAngle={3}
           animate={{ duration: 1000 }}
           endAngle={angle}
           labels={() => ''}
+          labelRadius={PIE_INNER_RADIUS + 8}
           style={{
             labels: { ...ticksLabelsStyle },
             data: {
               fill: ({ datum }) => (datum.tag ? datum.tag.color : colorUsage.error),
             },
           }}
-          data={props.data.map(pie => ({ ...pie, label: pie.tag.code }))}
+          data={props.data.map(pie => {
+            console.log(JSON.stringify(pie));
+            return { ...pie, label: `${pie.tag.code}\n${((pie.y / total) * 100).toFixed(0)}%` };
+          })}
         />
       )}
     </PieChartContainer>
